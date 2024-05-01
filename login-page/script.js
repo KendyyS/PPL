a2c4624277c9e957e99ae441057cda39c7cdfab2
Juan Hermanto <juanhermanto18@gmail.com>
2024-05-01 16:44:46 +0700

    Some modifications :)
    - Changed navbar color to white
    - Changed a few navbar buttons
    - Changed login-page input to User: Kennedy and Pass: Password
    - Moved the home image (taman UPH)
    - Added welcome text above the home image
    - Added icons next to the buttons
    - Added button functionality for Report linking to report.html and Logout linking to login-page.html
    - And probably a few others kinda forgot lol but  I think you can see the changes made :)

--- login-page/script.js
+++ login-page/script.js
@@ -5,7 +5,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     var password = document.getElementById('password').value;
     
     // Here you can implement your login logic, for now just a simple check
-    if (username === 'user' && password === 'password') {
+    if (username === 'Kennedy' && password === 'Password') {
         // Successful login, you can redirect to another page or show a success message
         window.location.href = "/ULNF Website/index.html";
     } else {
