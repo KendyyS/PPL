69c1a4da71bfdf1b18eabc15cb161f8b2b21d545
KendyyS <kennedysuganto@gmail.com>
2024-05-01 20:07:03 +0700

    claim form updated

--- Necessary/login-page/script.js
+++ Necessary/login-page/script.js
@@ -7,7 +7,8 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     // Here you can implement your login logic, for now just a simple check
     if (username === 'Kennedy' && password === 'Password') {
         // Successful login, you can redirect to another page or show a success message
-        window.location.href = "/ULNF Website/index.html";
+        // window.location.href = "/ULNF Website/index.html";
+        location.replace("/ULNF Website/index.html");
     } else {
         // Display error message for invalid credentials
         document.getElementById('error-message').textContent = 'Invalid username or password';
